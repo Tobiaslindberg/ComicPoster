200559389a7855c7a93386f3c17c535cccb38aad
Tobias Lindberg <Tobias@tlindberg.se>
2017-04-08 15:20:22 +0200

    Cleaned up Smbc provider

--- ComicPoster.Smbc/SmbcProvider.cs
+++ ComicPoster.Smbc/SmbcProvider.cs
@@ -14,26 +14,79 @@ namespace ComicPoster.Smbc
 
         public Comic DownloadComic(string oldId)
         {
-            var webclient = new WebClient();
-            
-            var html = webclient.DownloadString(ComicUrl);
-            
-            var document = new HtmlDocument();
-            document.LoadHtml(html);
+            var document = DownloadDocument();
 
-            var comicBody = document.GetElementbyId("cc-comicbody");
-            var images = comicBody.Descendants()
-                .Where(x => x.Name.Equals("img", StringComparison.OrdinalIgnoreCase));
-            var permaLinkText = document.GetElementbyId("permalinktext");
-            var permaLink = permaLinkText.Attributes.FirstOrDefault(x => x.Name.Equals("value", StringComparison.OrdinalIgnoreCase))?.Value;
-            var permaLinkUri = permaLink != null ? new Uri(permaLink) : null;
+            var permaLinkUri = GetPermaLink(document);
 
-            if (permaLinkUri == null 
-                || oldId.Equals(permaLinkUri?.ToString(), StringComparison.OrdinalIgnoreCase))
+            if (permaLinkUri == null
+                || oldId.Equals(permaLinkUri.ToString(), StringComparison.OrdinalIgnoreCase))
             {
                 return null;
             }
 
+            var images = GetImageNodes(document);
+            var title = GetTitle(permaLinkUri);
+
+            var comic = new Comic
+            {
+                Id = permaLinkUri.ToString(),
+                ComicUrl = new Uri(ComicUrl),
+                Name = "Saturday Morning Breakfast Cereal",
+                Title = title,
+                PermaLink = permaLinkUri,
+                ComicImages = new List<ComicImage>()
+            };
+
+            foreach (var htmlNode in images)
+            {
+                var image = CreateImage(htmlNode);
+
+                if (image == null)
+                {
+                    continue;
+                }
+
+                comic.ComicImages.Add(image);
+            }
+
+            return !comic.ComicImages.Any() ? null : comic;
+        }
+
+        private static ComicImage CreateImage(HtmlNode htmlNode)
+        {
+            var src =
+                htmlNode.Attributes.FirstOrDefault(x => x.Name.Equals("src", StringComparison.OrdinalIgnoreCase))?
+                    .Value;
+            var altText =
+                htmlNode.Attributes.FirstOrDefault(x => x.Name.Equals("title", StringComparison.OrdinalIgnoreCase))?
+                    .Value;
+
+            if (string.IsNullOrEmpty(src))
+            {
+                return null;
+            }
+
+            return new ComicImage
+            {
+                AltText = HttpUtility.HtmlDecode(altText),
+                ImageUrl = new Uri(src)
+            };
+        }
+
+        private static HtmlDocument DownloadDocument()
+        {
+            using (var webClient = new WebClient())
+            {
+                var html = webClient.DownloadString(ComicUrl);
+
+                var document = new HtmlDocument();
+                document.LoadHtml(html);
+                return document;
+            }
+        }
+
+        private static string GetTitle(Uri permaLinkUri)
+        {
             var title = permaLinkUri.ToString().Split('/').Last();
             if (string.IsNullOrEmpty(title))
             {
@@ -45,34 +98,28 @@ namespace ComicPoster.Smbc
                 title = title.First().ToString().ToUpper() + title.Substring(1);
             }
 
-            var comic = new Comic
-            {
-                Id = permaLinkUri.ToString(),
-                ComicUrl = new Uri(ComicUrl),
-                Name = "Saturday Morning Breakfast Cereal",
-                Title = title,
-                PermaLink = permaLinkUri,
-                ComicImages = new List<ComicImage>(),
-            };
+            return title;
+        }
 
-            foreach (var htmlNode in images)
-            {
-                var src = htmlNode.Attributes.FirstOrDefault(x => x.Name.Equals("src", StringComparison.OrdinalIgnoreCase))?.Value;
-                var altText = htmlNode.Attributes.FirstOrDefault(x => x.Name.Equals("title", StringComparison.OrdinalIgnoreCase))?.Value;
+        private static Uri GetPermaLink(HtmlDocument document)
+        {
+            var permaLinkText = document.GetElementbyId("permalinktext");
+            var permaLink = permaLinkText.Attributes
+                .FirstOrDefault(x => x.Name.Equals("value", StringComparison.OrdinalIgnoreCase))?.Value;
 
-                if (string.IsNullOrEmpty(src))
-                {
-                    continue;
-                }
+            var permaLinkUri = permaLink != null ? new Uri(permaLink) : null;
 
-                comic.ComicImages.Add(new ComicImage
-                {
-                    AltText = HttpUtility.HtmlDecode(altText),
-                    ImageUrl = new Uri(src)
-                });
-            }
+            return permaLinkUri;
+        }
 
-            return !comic.ComicImages.Any() ? null : comic;
+        private static IEnumerable<HtmlNode> GetImageNodes(HtmlDocument document)
+        {
+            var comicBody = document.GetElementbyId("cc-comicbody");
+
+            var images = comicBody.Descendants()
+                .Where(x => x.Name.Equals("img", StringComparison.OrdinalIgnoreCase));
+
+            return images;
         }
     }
 }
