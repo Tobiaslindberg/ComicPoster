107e75a1f53f2b7f0b5a07915a90f4646d716273
Tobias Lindberg <Tobias@tlindberg.se>
2017-04-08 15:26:48 +0200

    Cleaned up Xkcd provider

--- ComicPoster.Xkcd/XkcdProvider.cs
+++ ComicPoster.Xkcd/XkcdProvider.cs
@@ -15,25 +15,13 @@ namespace ComicPoster.Xkcd
 
         public Comic DownloadComic(string oldId)
         {
-            var webclient = new WebClient();
-            var html = webclient.DownloadString(ComicUrl);
+            var document = DownloadDocument();
 
-            var document = new HtmlDocument();
-            document.LoadHtml(html);
-
-            var comicBody = document.GetElementbyId("middleContainer");
-            var htmlNodes = comicBody.Descendants().ToList();
-            var imageUrlNode = htmlNodes.FirstOrDefault(x => x.InnerText.Contains("hotlinking/embedding"));
-            var permLinkNode = htmlNodes.FirstOrDefault(x => x.InnerText.Contains("Permanent link "));
+            var htmlNodes = GetHtmlNodes(document);
+            var imageUrl = GetImageUrl(htmlNodes);
+            var permLink = GetPermLink(htmlNodes);
+            var altText = GetAltText(htmlNodes);
             var comicTitle = htmlNodes.FirstOrDefault(x => x.Id.Equals("ctitle", StringComparison.OrdinalIgnoreCase))?.InnerText;
-            var comicDiv = htmlNodes.FirstOrDefault(x => x.Id.Equals("comic", StringComparison.OrdinalIgnoreCase));
-            var imageDiv = comicDiv?.Descendants().FirstOrDefault(x => x.Name.Equals("img", StringComparison.OrdinalIgnoreCase));
-
-            var imageUrl = GetLinks(imageUrlNode?.InnerText).FirstOrDefault();
-            var permLink = GetLinks(permLinkNode?.InnerText).FirstOrDefault();
-            var altText =
-                imageDiv?.Attributes.FirstOrDefault(x => x.Name.Equals("title", StringComparison.OrdinalIgnoreCase))?
-                    .Value;
 
             if (string.IsNullOrEmpty(imageUrl) 
                 || string.IsNullOrEmpty(permLink)
@@ -62,6 +50,53 @@ namespace ComicPoster.Xkcd
             return comic;
         }
 
+        private static string GetAltText(IEnumerable<HtmlNode> htmlNodes)
+        {
+            var comicDiv = htmlNodes.FirstOrDefault(x => x.Id.Equals("comic", StringComparison.OrdinalIgnoreCase));
+            var imageDiv = comicDiv?.Descendants().FirstOrDefault(x => x.Name.Equals("img", StringComparison.OrdinalIgnoreCase));
+
+            var altText =
+                imageDiv?.Attributes.FirstOrDefault(x => x.Name.Equals("title", StringComparison.OrdinalIgnoreCase))?
+                    .Value;
+
+            return altText;
+        }
+
+        private string GetPermLink(IEnumerable<HtmlNode> htmlNodes)
+        {
+            var permLinkNode = htmlNodes.FirstOrDefault(x => x.InnerText.Contains("Permanent link "));
+            var permLink = GetLinks(permLinkNode?.InnerText).FirstOrDefault();
+
+            return permLink;
+        }
+
+        private string GetImageUrl(IEnumerable<HtmlNode> htmlNodes)
+        {
+            var imageUrlNode = htmlNodes.FirstOrDefault(x => x.InnerText.Contains("hotlinking/embedding"));
+            var imageUrl = GetLinks(imageUrlNode?.InnerText).FirstOrDefault();
+
+            return imageUrl;
+        }
+
+        private static List<HtmlNode> GetHtmlNodes(HtmlDocument document)
+        {
+            var comicBody = document.GetElementbyId("middleContainer");
+            var htmlNodes = comicBody.Descendants().ToList();
+
+            return htmlNodes;
+        }
+
+        private static HtmlDocument DownloadDocument()
+        {
+            var webclient = new WebClient();
+            var html = webclient.DownloadString(ComicUrl);
+
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            return document;
+        }
+
         private IEnumerable<string> GetLinks(string message)
         {
             if (string.IsNullOrEmpty(message))
@@ -69,9 +104,11 @@ namespace ComicPoster.Xkcd
                 return null;
             }
 
-            var urlRx = new Regex(@"((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase);
+            var urlRegex = new Regex(
+                @"((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*",
+                RegexOptions.IgnoreCase);
+            var matches = urlRegex.Matches(message);
 
-            var matches = urlRx.Matches(message);
             return (from Match match in matches select match.Value).ToList();
         }
     }
